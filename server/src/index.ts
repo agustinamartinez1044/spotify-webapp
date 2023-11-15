49dfb31657849bd99775a05e2916ac0f7cd58c21
agustinamartinez1044 <agustina.martinez@tiendamia.com>
2023-11-15 14:19:58 -0300

    fix(index): sync database and initiate server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,19 @@
-import './pre-start'; // Must be the first import
+import './pre-start';
 import logger from 'jet-logger';
-
 import EnvVars from '@src/constants/EnvVars';
 import server from './server';
+import sequelize from './services/database/sequelize';
 
 
 // **** Run **** //
 
-const SERVER_START_MSG = ('Express server started on port: ' + 
+const SERVER_START_MSG = ('Express server started on port: ' +
   EnvVars.Port.toString());
+sequelize.sync({force: false}).then(() => {
 
-server.listen(EnvVars.Port, () => logger.info(SERVER_START_MSG));
+    const port = process.env.PORT || 5000;
+    server.listen(EnvVars.Port, () => logger.info(SERVER_START_MSG));
+})
+    .catch((error) => {
+        console.error('Error durante la sincronización de la base de datos:', error);
+    });
