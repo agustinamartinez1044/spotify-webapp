ca8962ed93dd00a3207f1982de0198ecacc7b198
agustinamartinez1044 <agustina.martinez@tiendamia.com>
2023-11-15 16:42:44 -0300

    refactor(NavBar): link is added to redirect home

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import {FaHome, FaRegListAlt, FaSearch} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 import './navBar.css';
 
 const NavBar = () => {
     return (
         <div className="nav-container">
             <div className="nav-item-container">
-                <div className="nav-item">
+                <Link to="/" className="nav-item">
                     <FaHome/>
                     <span>Home</span>
-                </div>
+                </Link>
                 <div className="nav-item">
                     <FaSearch/>
                     <span>Search</span>
