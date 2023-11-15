2302321375ae2c52b278d5531898816f63cb1893
agustinamartinez1044 <agustina.martinez@tiendamia.com>
2023-11-15 02:39:18 -0300

    refactor(SpotifyServices): defining type and parameters

--- server/src/services/SpotifyService.ts
+++ server/src/services/SpotifyService.ts
@@ -1,15 +1,12 @@
-import UserRepo from '@src/repos/UserRepo';
-import { IUser } from '@src/models/User';
-import { RouteError } from '@src/other/classes';
-import HttpStatusCodes from '@src/constants/HttpStatusCodes';
+import {SporifyArtist, SpotifyAlbum} from "@src/routes/types/types";
 
 export const USER_NOT_FOUND_ERR = 'User not found';
 
-function searchArtist(): Promise<void> {
+function searchArtist(artistName: string): Promise<SporifyArtist> {
   return Promise.resolve();
 }
 
-function getArtistAlbums(): Promise<void> {
+function getArtistAlbums(artistId: string, artistName: string): Promise<SpotifyAlbum> {
   return Promise.resolve();
 }
 
