11087ebc8fc31784d21addc6fb67792ab94ebb0e
agustinamartinez1044 <agustina.martinez@tiendamia.com>
2023-11-15 02:56:36 -0300

    feat(SpotifyServices): using the Spotify API to look up artist ID

--- server/src/services/SpotifyService.ts
+++ server/src/services/SpotifyService.ts
@@ -1,9 +1,22 @@
 import {SporifyArtist, SpotifyAlbum} from "@src/routes/types/types";
+import axios, {AxiosResponse} from 'axios';
+import {getAccessToken} from './authService';
 
 export const USER_NOT_FOUND_ERR = 'User not found';
+const api_url = process.env.SPOTIFY_API_URL;
 
-function searchArtist(artistName: string): Promise<SporifyArtist> {
-  return Promise.resolve();
+async function searchArtist(artistName: string): Promise<SporifyArtist> {
+    const accessToken = getAccessToken();
+    const response: AxiosResponse<SporifyArtist> = await axios.get(`${api_url}/search`, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+            q: artistName,
+            type: 'artist',
+        },
+    });
+    return response.data;
 }
 
 function getArtistAlbums(artistId: string, artistName: string): Promise<SpotifyAlbum> {
