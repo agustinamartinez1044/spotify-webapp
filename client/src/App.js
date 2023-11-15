0cca416f2ec0c9a3071d1519001ead4e1cd10689
agustinamartinez1044 <agustina.martinez@tiendamia.com>
2023-11-15 16:27:11 -0300

    refactor(App): incorporate AlbumList component to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home.jsx'
 import SearchBar from "./components/SearchBar/SearchBar.jsx";
 import NavBar from "./components/NavBar/NavBar.jsx";
+import AlbumList from "./components/AlbumList/AlbumList.jsx";
 
 function App() {
   return (
     <div className="App">
         <SearchBar/>
-        <Home/>
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route exact path="/albums" element={<AlbumList/>}/>
+                </Routes>
+            </div>
+        </Router>
         <NavBar/>
     </div>
   );
