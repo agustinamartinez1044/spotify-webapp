97c468d35e70d0fe08a70a88129ef6068edafa53
agustinamartinez1044 <agustina.martinez@tiendamia.com>
2023-11-15 15:34:31 -0300

    refactor(app): refactor app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home/Home.js'
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello World!</h1>
+        <Home/>
     </div>
   );
 }
