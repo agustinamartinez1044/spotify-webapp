8d71172f6787ac9ccdddab57a1a17b8cef97a0c7
agustinamartinez1044 <agustina.martinez@tiendamia.com>
2023-11-15 02:36:07 -0300

    refactor(SpotifyController): adjusting functionality

--- server/src/controllers/SpotifyController.ts
+++ server/src/controllers/SpotifyController.ts
@@ -1,42 +1,18 @@
-import {IReq, IRes} from '../routes/types/types';
 import SpotifyService from '../services/SpotifyService';
-import HttpStatusCodes from '../constants/HttpStatusCodes';
-import {NextFunction, Request, Response} from 'express';
 
-async function getAlbumData(req: Request, res: Response, next: NextFunction) {
-    const artistName = req.query.artistName as string;
-
-    if (!artistName) {
-        return res.status(HttpStatusCodes.BAD_REQUEST).json({error: "Artist name is required"});
+async function getArtistAlbumsData(artistName: string) {
+    const searchResult = await SpotifyService.searchArtist(artistName);
+    if (searchResult.artists.items.length === 0) {
+        throw new Error("Artist not found");
     }
-
-    try {
-        const searchResult = await SpotifyService.searchArtist(artistName);
-        if (searchResult.artists.items.length === 0) {
-            return res.status(HttpStatusCodes.NOT_FOUND).json({error: "Artist not found"});
-        }
-
-        const artist = searchResult.artists.items[0];
-
-        res.json(artist);
-    } catch (error) {
-    }
+    return searchResult.artists.items[0];
 }
 
-
-function searchAlbum(req: IReq, res: IRes) {
-    try {
-        const artistId = req.params.id;
-        if (!artistId) {
-            return res.status(HttpStatusCodes.BAD_REQUEST).json({error: 'Artist ID is required'});
-        }
-        const albums = SpotifyService.getArtistAlbums(artistId, '');
-        res.json(albums);
-    } catch (error) {
-    }
+async function searchAlbum(artistId: string) {
+    return await SpotifyService.getArtistAlbums(artistId, '');
 }
 
 export default {
     searchAlbum,
-    getAlbumData
+    getArtistAlbumsData
 } as const;
