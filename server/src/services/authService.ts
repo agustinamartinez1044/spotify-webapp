2b78da25176005b531276da2824e7e1841ef7823
agustinamartinez1044 <agustina.martinez@tiendamia.com>
2023-11-15 02:51:49 -0300

    feat(authService): add function requestNewAccessToken

--- server/src/services/authService.ts
+++ server/src/services/authService.ts
@@ -5,9 +5,37 @@ const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const tokenUrl = process.env.SPOTIFY_TOKEN_URL;
 
+let accessToken: string;
+let tokenExpirationTime: number;
 function getEncodedCredentials(clientId: string, clientSecret: string): string {
     if (!clientId || !clientSecret) {
         throw new Error('Client ID or Client Secret is not set.');
     }
     return Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+}
+
+async function requestNewAccessToken(): Promise<string> {
+    if (!client_id || !client_secret || !tokenUrl) {
+        throw new Error('Client ID or Client Secret is undefined.');
+    }
+
+    const encodedCredentials = getEncodedCredentials(client_id, client_secret);
+
+    try {
+        const response = await axios.post(tokenUrl, 'grant_type=client_credentials', {
+            headers: {
+                'Authorization': `Basic ${encodedCredentials}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        const {access_token, expires_in} = response.data;
+        accessToken = access_token;
+        tokenExpirationTime = new Date().getTime() + expires_in * 1000;
+        return accessToken;
+    }
+    catch (error) {
+        logger.err((`Error obteniendo el token de acceso de Spotify: ${error}`));
+        throw new Error('Error al obtener el token de acceso de Spotify');
+    }
 }
