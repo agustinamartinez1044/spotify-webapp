64544b03256d53757411561bd28fb564901c06bf
agustinamartinez1044 <agustina.martinez@tiendamia.com>
2023-11-15 16:45:54 -0300

    refactor(AlbumCard): adjusts html tags

--- client/src/components/AlbumCard/AlbumCard.jsx
+++ client/src/components/AlbumCard/AlbumCard.jsx
@@ -8,13 +8,15 @@ const AlbumCard = ({album}) => {
     return (
         <div className="card-container">
             <div className="album-card">
-                <div className="card-content"></div>
                 <div className="card-image">
-                    <img src={image} alt={name}/></div>
-                <h3 className="card-title">{name}</h3>
-                <div class="card-text">
-                    <div key={id}>{artistName}</div>
-                    <p>{release_date}</p>
+                    <img src={image} alt={name}/>
+                </div>
+                <div className="card-content">
+                    <h3 className="card-title">{name}</h3>
+                    <div class="card-text">
+                        <div key={id}>{artistName}</div>
+                        <p>{release_date}</p>
+                    </div>
                 </div>
             </div>
         </div>
