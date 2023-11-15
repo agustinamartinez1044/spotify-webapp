bce50ecfff21701d7ecd714f87e2ef71bdb7b41c
agustinamartinez1044 <agustina.martinez@tiendamia.com>
2023-11-15 00:35:55 -0300

    refactor(api): adjust necessary routes

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
 import jetValidator from 'jet-validator';
+import SpotifyRoutes from './SpotifyRoutes';
 
 import Paths from '../constants/Paths';
-import User from '@src/models/User';
-import UserRoutes from './UserRoutes';
 
 
 // **** Variables **** //
@@ -14,37 +13,21 @@ const apiRouter = Router(),
 
 // ** Add UserRouter ** //
 
-const userRouter = Router();
+const spotifyRouter = Router();
 
-// Get all users
-userRouter.get(
-  Paths.Users.Get,
-  UserRoutes.getAll,
+
+spotifyRouter.get(
+  Paths.Artist.Search,
+    SpotifyRoutes.searchArtist,
 );
 
-// Add one user
-userRouter.post(
-  Paths.Users.Add,
-  validate(['user', User.isUser]),
-  UserRoutes.add,
+spotifyRouter.get(
+    Paths.Album.Get,
+    SpotifyRoutes.getAlbumData
 );
 
-// Update one user
-userRouter.put(
-  Paths.Users.Update,
-  validate(['user', User.isUser]),
-  UserRoutes.update,
-);
 
-// Delete one user
-userRouter.delete(
-  Paths.Users.Delete,
-  validate(['id', 'number', 'params']),
-  UserRoutes.delete,
-);
-
-// Add UserRouter
-apiRouter.use(Paths.Users.Base, userRouter);
+apiRouter.use(Paths.Artist.Base, spotifyRouter);
 
 
 // **** Export default **** //
