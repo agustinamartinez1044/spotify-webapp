8acf3259a2e8501c1b7f20fcac20a4930d3357cb
agustinamartinez1044 <agustina.martinez@tiendamia.com>
2023-11-15 15:48:46 -0300

    refactor(App): incorporate Home and SearchBar components to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
-import Home from './pages/Home/Home.js'
+import Home from './pages/Home/Home.jsx'
+import SearchBar from "./components/SearchBar/SearchBar.jsx";
 
 function App() {
   return (
     <div className="App">
+        <SearchBar/>
         <Home/>
     </div>
   );
