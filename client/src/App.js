034a37da1cf4453314d4154833481343e65bb3cc
agustinamartinez1044 <agustina.martinez@tiendamia.com>
2023-11-15 16:03:55 -0300

    refactor(App): incorporate NavBar component to App

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './pages/Home/Home.jsx'
 import SearchBar from "./components/SearchBar/SearchBar.jsx";
+import NavBar from "./components/NavBar/NavBar.jsx";
 
 function App() {
   return (
     <div className="App">
         <SearchBar/>
         <Home/>
+        <NavBar/>
     </div>
   );
 }
