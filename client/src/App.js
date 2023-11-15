9f28d1e905f2d2dedf56af987eab13f3d45d64b0
agustinamartinez1044 <agustina.martinez@tiendamia.com>
2023-11-15 16:44:10 -0300

    refactor(App): integrate routing with album state and navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {BrowserRouter as Router, Route, Routes,useNavigate} from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home.jsx'
 import SearchBar from "./components/SearchBar/SearchBar.jsx";
@@ -7,20 +7,31 @@ import NavBar from "./components/NavBar/NavBar.jsx";
 import AlbumList from "./components/AlbumList/AlbumList.jsx";
 
 function App() {
+    const [albums, setAlbums] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (albums.length > 0) {
+            navigate('/albums');
+        }
+    }, [albums, navigate]);
+
   return (
     <div className="App">
-        <SearchBar/>
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route exact path="/" element={<Home/>}/>
-                    <Route exact path="/albums" element={<AlbumList/>}/>
-                </Routes>
-            </div>
-        </Router>
+        <SearchBar setAlbums={setAlbums}/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/albums" element={<AlbumList albums={albums}/>}/>
+        </Routes>
         <NavBar/>
     </div>
   );
 }
 
-export default App;
+export default function AppWithRouter() {
+    return (
+        <Router>
+            <App/>
+        </Router>
+    );
+}
